59fc1e3f3e008070db61fb3d35b972d89721cc25
lanastasov <lubomir.anastasov@gmail.com>
2021-02-05 09:37:44 +0200

    add components

--- 02-personal-finance/src/App.js
+++ 02-personal-finance/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import Header from './components/Header';
+import TransactionTable from './components/TransactionTable';
+import Chart from './components/Chart';
+import AddCategory from './components/AddCategory';
+import AddTransaction from './components/AddTransaction';
+
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <TransactionTable />
+      <Chart />
+      <AddCategory />
+      <AddTransaction />
+    </>
+  )
 }
-
-export default App;
---
+++ 02-personal-finance/src/components/TransactionTable.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function TransactionTable(){
+    return <h1>TransactionTable component</h1>
+}
